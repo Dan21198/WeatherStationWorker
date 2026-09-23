2193992f475215358784a54994dce5c8d2cea354
Daniel Hořejší <120517739+Dan21198@users.noreply.github.com>
2026-09-23 22:57:02 +0200

    Update error messages for URL validation

--- WeatherStationWorker/WeatherSettings.cs
+++ WeatherStationWorker/WeatherSettings.cs
@@ -4,7 +4,7 @@ namespace WeatherStationWorker;
 
 public class WeatherSettings
 {
-    [Required(ErrorMessage = "URL adresa meteostanice chybí v konfiguraci.")]
-    [Url(ErrorMessage = "Zadaná hodnota není platná URL adresa.")]
+    [Required(ErrorMessage = "The weather station URL is missing in the configuration.")]
+    [Url(ErrorMessage = "The entered value is not a valid URL.")]
     public string Url { get; set; } = string.Empty;
 }
